794f56c43b22d0a59deb182e98f1d4687b3f1ad9
vasudevelam <vasudevelam@gmail.com>
2025-09-03 21:21:34 +0530

    class Attendance calculate attendance % if %<75 student is not allowed

---
+++ If-else-ques/ouestion4.py
@@ -0,0 +1,6 @@
+#wap to the number of class and attendence . clculate atten % if %<75 student is not allowed
+att=int(input("Enter a Attendence is:  "))
+if att>=75:
+    print("This student was allowed for exam")
+else:
+    print("This student was not allowed for exam")    
