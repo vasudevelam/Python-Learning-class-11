a53f90a6a6a7cbb059f1b31355c64bf00435696d
vasudevelam <vasudevelam@gmail.com>
2025-09-03 21:28:29 +0530

    #wap to check a number is positive , negative, or zero

---
+++ If-else-ques/ouestion10.py
@@ -0,0 +1,8 @@
+#wap to check a number is positive , negative, or zero
+num=int(input("Enyer a number is:  "))
+if num>=0:
+    print(num,"This is a number possitive number ")
+elif num<=0:
+    print(num,"This is a number negative number ")
+elif num==0:
+    print(num,"This is a number zero ")
