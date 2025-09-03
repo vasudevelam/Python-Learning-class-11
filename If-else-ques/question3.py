f25e90d2c9b6738093685ae694bbc8d5988ad24f
vasudevelam <vasudevelam@gmail.com>
2025-09-03 14:51:38 +0530

    QUESTION 3

---
+++ If-else-ques/question3.py
@@ -0,0 +1,7 @@
+#wap to check a person is eligible for a driving licences
+age=int(input("Enter a number is:  "))
+if (age>18):
+    print(age,"this person is eligible for driving licences")
+else:
+    print(age,"this person is not eligible for driving licences")
+    
